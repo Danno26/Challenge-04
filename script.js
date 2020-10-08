91a118b812c600db0b2ff05f7c8a1c60b8373e0d
Danno26 <55775106+Danno26@users.noreply.github.com>
2020-10-07 17:55:44 -0700

    created some questions

---
+++ script.js
@@ -0,0 +1,26 @@
+//question list
+var questions = [{
+        question: "Question One: NAAARRRUUUTTTOOOOOOO!!!!"
+        choice: ["Rasengan!", "Bunshin No Jussu!", "SAAASSSSUUUKKKEEEE", "Haremu No Jutsu!"]
+        answer: "SAAASSSSUUUKKKEEEE"
+    },
+    {
+        question: "Pi is:"
+        choice: ["= 3.14", "Yummy!", "Infinity", "= 3.141592"]
+        answer: "Infinity"
+    }, {
+        question: "“An apple a day keeps any one away, if you throw it hard enough.” "
+        choice: ["Marie Mjolnir", "Maka Albarn", "Dr. Franken Stein", "What?"]
+        answer: "Marie Mjolnir"
+    }, {
+        question: "Strongest type of Bond: "
+        choice: ["blood pact", "Bro", "James", "Ionic"]
+        answer: "Ionic"
+    }, {
+        question: "Ashs's first Pokemon was: "
+        choice: ["Yes.", "Pikachu", "Bulbasoar", "idk man"]
+        answer: "Pikachu"
+    },
+
+
+]
